68dc0d0c7eaed1fb75cfcd1a9625bb5e27b49a8a
Leo-037 <leobaraldi2000@gmail.com>
2022-04-28 17:21:11 +0200

    Basic new Popup

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,51 +1,26 @@
 import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
+import { ISavedList } from "./interfaces";
 
 const Popup = () => {
-  const [count, setCount] = useState(0);
-  const [currentURL, setCurrentURL] = useState<string>();
+  const [savedLists, setSavedLists] = useState<ISavedList[]>([]);
 
   useEffect(() => {
-    chrome.action.setBadgeText({ text: count.toString() });
-  }, [count]);
-
-  useEffect(() => {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      setCurrentURL(tabs[0].url);
-    });
-  }, []);
-
-  const changeBackground = () => {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      const tab = tabs[0];
-      if (tab.id) {
-        chrome.tabs.sendMessage(
-          tab.id,
-          {
-            color: "#555555",
-          },
-          (msg) => {
-            console.log("result message:", msg);
-          }
-        );
-      }
-    });
-  };
+    chrome.storage.sync.get('saved_lists', (result) => setSavedLists(result.saved_lists));
+  });
 
   return (
-    <>
-      <ul style={{ minWidth: "700px" }}>
-        <li>Current URL: {currentURL}</li>
-        <li>Current Time: {new Date().toLocaleTimeString()}</li>
-      </ul>
-      <button
-        onClick={() => setCount(count + 1)}
-        style={{ marginRight: "5px" }}
-      >
-        count up
-      </button>
-      <button onClick={changeBackground}>change background</button>
-    </>
+    <div>
+      <h2>Saved Deck Lists: </h2>
+      <h3>
+        <base target="_blank" />
+        <ul style={{ minWidth: "700px" }}>
+          {savedLists.map((list: ISavedList, i) => (
+            <li key={i}><a style={{color: 'black', textDecoration: "none"}} href={list.url} dangerouslySetInnerHTML={{ __html: list.title }}></a></li>
+          ))}
+        </ul>
+      </h3>
+    </div>
   );
 };
 
